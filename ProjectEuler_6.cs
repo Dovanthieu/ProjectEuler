77394b909135da237af50ea253e2551b2432b217
ThieuBka <thieubka@gmail.com>
2019-09-27 22:24:40 +0700

    10.25PM_27/9/2019
    ProjectEuler_6 with C#

---
+++ ProjectEuler_6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler_6
+{
+    class Program
+    {
+        static int SumSqrt(int n)
+        {
+            int sumsqrt = 0;
+            for (int i = 1;i<=n;i++)
+            {
+                sumsqrt += i * i;
+            }
+            return (int)sumsqrt;
+        }
+        static int SqrtSum(int n)
+        {
+            int sqrtsum = ((n*(n + 1)) / 2)* ((n * (n + 1)) / 2);
+            return sqrtsum;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 100;
+            Console.WriteLine("Result ProjectEuler_6 is {0}", SqrtSum(n) - SumSqrt(n));
+            Console.ReadKey();
+        }
+    }
+}
