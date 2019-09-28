314b3c6d1217cf62c6ffbf7c4c9df75eb679a424
ThieuBka <thieubka@gmail.com>
2019-09-28 18:00:00 +0700

    6PM_28/9/2019
    ProjectEuler_5 with C#

---
+++ ProjectEuler_5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler_5
+{
+    class Program
+    {
+        static int gcd(int a ,int b)
+        {
+            if (a == 0)
+                return b;
+            return gcd(b % a, a);
+            
+        }
+        static int lcm(int n)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+               result *= i / gcd(i, result);
+
+            }
+            return (int)result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Result ProjectEuler_5 is {0}", lcm(20));
+            Console.ReadKey();
+        }
+    }
+}
