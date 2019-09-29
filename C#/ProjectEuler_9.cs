9a4fb9770ab1bed33edd4362ac1bc8ca697b4cde
ThieuBka <thieubka@gmail.com>
2019-09-29 16:39:42 +0700

    4.40PM_29/9/2019
    ProjectEuler_9 with C#

---
+++ C#/ProjectEuler_9.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler_9
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int n = 1000;
+            int i = 0;
+            int j = n / 3;
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            for (i = 0; i <= (n / 3); i++)
+                for (j = n / 3; ((n / 3) <= j) && (j <= n); j++)
+                    if (Math.Pow(n - i - j, 2) + Math.Pow(i, 2) == Math.Pow(j, 2))
+                    {
+                        a = i;
+                        b = n - i - j;
+                        c = j;
+                    }
+            Console.WriteLine("Result ProjectEuler_9 is {0},{1},{2}", a,b,c);
+            Console.ReadKey();
+
+        }
+    }
+}
