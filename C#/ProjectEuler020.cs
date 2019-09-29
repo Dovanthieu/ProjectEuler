9271d6eac901c4d4999d96e209f57093d92e0b40
ThieuBka <thieubka@gmail.com>
2019-09-29 22:05:07 +0700

    10.05PM_29/9/2019
    ProjectEuler_20 with C#

---
+++ C#/ProjectEuler020.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ProjectEuler_20
+{
+    class Program
+    {
+        static int Sum(int n)
+        {
+            int sum= 0;
+            BigInteger Factorial = 1;
+            for (int i = 2; i <= 100; i++)
+            {
+                Factorial *= i;
+            }
+            while (Factorial > 0)
+            {
+                sum += (int)(Factorial % 10);
+                Factorial /= 10;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            int n = 100;
+            Console.WriteLine("Result ProjectEuler_20 is {0}", Sum(n));
+            Console.ReadKey();
+        }
+    }
+}
