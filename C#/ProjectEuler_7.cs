52d558e8eebf7a06c6b0f8d66d098c3ab9be768f
ThieuBka <thieubka@gmail.com>
2019-09-29 15:01:50 +0700

    3PM_29/9/2019
    ProjectEuler_7 with C#

---
+++ C#/ProjectEuler_7.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler_7
+{
+    class Program
+    {
+        public static int FloorSqrt(int n)
+        {
+            double i = Math.Sqrt(n);
+            return (int)i;
+        }
+        static bool IsPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+            else if (n <= 3)
+                return true;
+            else if(n % 2 == 0)
+                return false;
+            else
+            {
+                for (int i = 3; i <= FloorSqrt(n) + 1; i += 2)
+                    if (n % i == 0)
+                        return false;
+                return true;
+            }
+        }
+        static int PrimeIndexN(int n)
+        {
+            int PrimeNum = 1;
+            int count = 1;
+            while(PrimeNum<10001)
+            {
+                count += 2;
+                if (IsPrime(count))
+                {
+                    PrimeNum++;
+                }
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            int n = 10001;
+            Console.WriteLine("Result ProjectEuler_7 is {0}", PrimeIndexN(n));
+            Console.ReadKey();
+        }
+    }
+}
